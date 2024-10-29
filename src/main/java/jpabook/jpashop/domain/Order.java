3a36fd57f32a20f106cb223a9e8cdec7f26a1906
nahye <nnhh.shine@gmail.com>
2024-10-29 17:05:22 +0900

    엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jdk.jfr.Unsigned;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="orders")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> oderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name="delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; // 주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+}
